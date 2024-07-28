e0e1d71092a4a3801b4dbbde9a04dcff14208624
Luc Galarneau <lgalarno@gmail.com>
2024-07-28 12:51:58 -0400

    paginate and filter in tables

--- static/js/data_tables.js
+++ static/js/data_tables.js
@@ -7,13 +7,15 @@ htmx.onLoad(function() {
             { orderable: false,
                 targets: -1 }
             ],
-        searching: false,
-        bPaginate: false,
+        searching: true,
+        bPaginate: true,
+        pageLength: 25,
         info: false,
         order: [],
         processing: true,
         deferRender: true,
-        bDestroy: true
+        bDestroy: true,
+        language: {"search": "Filter:"}
     });
 
     $('#table_pages').DataTable({
@@ -21,13 +23,15 @@ htmx.onLoad(function() {
             { orderable: false,
                 targets: -1 }
             ],
-        searching: false,
-        bPaginate: false,
+        searching: true,
+        bPaginate: true,
+        pageLength: 25,
         info: false,
         order: [],
         processing: true,
         deferRender: true,
-        bDestroy: true
+        bDestroy: true,
+        language: {"search": "Filter:"}
     });
     document.body.addEventListener('htmx:afterRequest', function(evt) {
         table_listDataTable.ajax.reload(function() {
